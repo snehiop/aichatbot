70c28c1bc89c629dfe4d77c3ba12c68fb4320ad1
Utsah <utsah@ch.iitr.ac.in>
2025-01-09 03:30:01 +0530

    changed urls for prod

--- jee_chatbot/urls.py
+++ jee_chatbot/urls.py
@@ -19,9 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('chatbot/admin/', admin.site.urls),
     path('chatbot/', include('chatbot.urls')),
-    path('', include('chatbot.urls')),  # Include URLs from the chatbot app
+    # path('', include('chatbot.urls')),  # Include URLs from the chatbot app
 ]
 
 
